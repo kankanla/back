af6eff916372f4ff69afb601572ebe4a2b453141
kankanla <koyi_s@hotmail.com>
2016-03-25 17:52:01 +0900

    WIN7 add drv for load
    WIN7 add drv for load

--- DISM.cmd
+++ DISM.cmd
@@ -1,10 +1,10 @@
-@echo off
+@echo off 
 rem 12:17 2016/03/25
 
 
 rem SETUP
 rem ////////////////////////////////////
-SET USBDRV=E:\
+SET USBDRV=F:\
 SET MOUNTDRV=C:\indrv\
 SET ADDDRV=%MOUNTDRV%ADDDrivers
 SET OSIMGDRV=OSIMG
@@ -12,32 +12,55 @@ SET IMAGEFILE=%USBDRV%sources\install.wim
 SET IMAGENAME="Windows 7 PROFESSIONAL"
 SET BOOTWIM=%USBDRV%sources\boot.wim
 
- 
+:setp1
 %MOUNTDRV%DISM\DISM /Unmount-Image /MountDir:%MOUNTDRV%%OSIMGDRV% /discard
 %MOUNTDRV%DISM\DISM /get-imageinfo /imagefile:%IMAGEFILE%
+
+rem Name : Windows 7 HOMEBASIC
+rem Description : Windows 7 HOMEBASIC
+rem Size : 11,681,373,841 bytes
+rem 
+rem Index : 2
+rem Name : Windows 7 HOMEPREMIUM
+rem Description : Windows 7 HOMEPREMIUM
+rem Size : 12,194,650,761 bytes
+rem 
+rem Index : 3
+rem Name : Windows 7 PROFESSIONAL
+rem Description : Windows 7 PROFESSIONAL
+rem Size : 12,106,260,814 bytes
+rem 
+rem Index : 4
+rem Name : Windows 7 ULTIMATE
+rem Description : Windows 7 ULTIMATE
+rem Size : 12,259,188,245 bytes
+
 %MOUNTDRV%DISM\DISM /mount-image /imagefile:%IMAGEFILE% /name:%IMAGENAME% /MountDir:%MOUNTDRV%%OSIMGDRV%
-%MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%%OSIMGDRV% /Add-Driver /Driver:%ADDDRV% /forceunsigned
+for /D /R %ADDDRV% %%a in (*) do %MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%%OSIMGDRV% /Add-Driver /Driver:%%a /forceunsigned
+rem  %MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%%OSIMGDRV% /Add-Driver /Driver:%ADDDRV% /forceunsigned
 %MOUNTDRV%DISM\DISM /Unmount-Image /MountDir:%MOUNTDRV%%OSIMGDRV% /Commit
 pause
 
-
-rem goto end
+:stup2
 %MOUNTDRV%DISM\DISM /get-imageinfo /imagefile:%BOOTWIM%
 
 echo "Microsoft Windows PE (x64)"
 %MOUNTDRV%DISM\DISM /Unmount-Image /MountDir:%MOUNTDRV%boot /Discard
 %MOUNTDRV%DISM\DISM /mount-image /imagefile:%BOOTWIM% /name:"Microsoft Windows PE (x64)" /mountdir:%MOUNTDRV%boot
-%MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Add-Driver /Driver:%ADDDRV% /forceunsigned
-%MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Add-Driver /Driver:%ADDDRV% /forceunsigned
+for /D /R %ADDDRV% %%a in (*) do %MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Add-Driver /Driver:%%a /forceunsigned
 %MOUNTDRV%DISM\DISM /Unmount-Image /MountDir:%MOUNTDRV%boot /Commit
+
+:step3
+%MOUNTDRV%DISM\DISM /get-imageinfo /imagefile:%BOOTWIM%
 
 echo "Microsoft Windows Setup (x64)"
 %MOUNTDRV%DISM\DISM /Unmount-Image /MountDir:%MOUNTDRV%boot /Discard
 %MOUNTDRV%DISM\DISM /mount-image /imagefile:%BOOTWIM% /name:"Microsoft Windows Setup (x64)" /mountdir:%MOUNTDRV%boot
-%MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Add-Driver /Driver:%ADDDRV% /forceunsigned
-%MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Add-Driver /Driver:%ADDDRV% /forceunsigned
+for /D /R %ADDDRV% %%a in (*) do %MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Add-Driver /Driver:%%a /forceunsigned
 %MOUNTDRV%DISM\DISM /Unmount-Image /MountDir:%MOUNTDRV%boot /Commit
 pause
+rem %MOUNTDRV%DISM\DISM /Image:%MOUNTDRV%boot /Remove-Driver /Driver:asmthub3.cat
+
 
 :end
 rem https://technet.microsoft.com/ja-jp/library/dd744355(v=ws.10).aspx
