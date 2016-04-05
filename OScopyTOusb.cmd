f03e90a6c4c3213436d140145d606c5bba3d6134
kankanla <koyi_s@hotmail.com>
2016-04-05 12:22:26 +0900

    Added files via upload

--- OScopyTOusb.cmd
+++ OScopyTOusb.cmd
@@ -3,19 +3,23 @@ rem 9:56 2016/03/30
 
 rem SETUP
 rem ////////////////////////////////////
-SET USBDRV=G
-SET ODDDRV=D
+SET USBDRV=D
+SET ODDDRV=E
 
 
 goto step1
 
 :step1
-format %USBDRV%: /fs:fat /v:FileToUsb  /x /q
+format %USBDRV%: /fs:fat32 /v:FileToUsb  /x /q
 
 
 :step2
 xcopy %ODDDRV%:* %USBDRV%: /e /g /h /r /y
 
+:step3
+rem UEFI Boot Support
+mkdir %USBDRV%:\efi\boot\
+copy %windir%\Boot\EFI\bootmgfw.efi %USBDRV%:\efi\boot\bootx64.efi
 
 
 
